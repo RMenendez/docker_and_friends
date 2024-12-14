56216be2244abd01abc72c2bbb5caa54306daa4b
RMenendez <rmgiacomini@gmail.com>
2024-12-14 20:26:46 +0100

    With unit and integration tests - needs local redis container up

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,12 +1,22 @@
 # src/page_tracker/app.py
 
+from functools import cache
+
 from flask import Flask
-from redis import Redis
+from redis import Redis, RedisError
 
 app = Flask(__name__)
-redis = Redis()
 
 @app.get("/")
 def index():
-    page_views = redis.incr("page_views")
-    return f"This page has been seen {page_views} times."
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{pensive face}", 500
+    else:
+        return f"This page has been seen {page_views} times."
+
+@cache
+def redis():
+    return Redis()
