72db04ac0b4aed0252fd1175c273819c6ce9b233
RMenendez <rmgiacomini@gmail.com>
2024-12-15 21:02:11 +0100

    After static code analysis (black, isort, pylint) and security scanning (bandit)

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,5 +1,5 @@
 # src/page_tracker/app.py
-
+''' Module for page_tracker app '''
 import os
 from functools import cache
 
@@ -8,16 +8,20 @@ from redis import Redis, RedisError
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def index():
+    ''' AppMain for page_tracker app '''
     try:
         page_views = redis().incr("page_views")
+        return f"This page has been seen {page_views} times."
+
     except RedisError:
         app.logger.exception("Redis error")
         return "Sorry, something went wrong \N{pensive face}", 500
-    else:
-        return f"This page has been seen {page_views} times."
+
 
 @cache
 def redis():
-    return Redis.from_url(os.getenv("REDIS_URL","redis://localhost:6379"))
+    ''' Function to connect to redis '''
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
